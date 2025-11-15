434be0a1b54c7545a3c6bfbce399bd4a8795ac14
DSergent1 <108247878+DSergent1@users.noreply.github.com>
2025-11-15 17:20:16 -0500

    Delete JavaDoc directory

--- JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.organism","l":"DatabaseHelper"},{"p":"com.organism","l":"Organism"},{"p":"com.organism","l":"OrganismApplication"},{"p":"com.organism","l":"OrganismGUI"},{"p":"com.organism","l":"OrganismManager"}];updateSearchResults();
